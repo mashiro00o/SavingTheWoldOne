0af92b3c96f7c7ef607904a1a7df57c6348c9889
Mashiro <125548786+mashiro00o@users.noreply.github.com>
2023-03-07 04:30:40 +0000

    updated web app with html, css and flask jinja

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello from Flask!'
-
+    data = None
+    numbers = [1, 2, 3, 4, 5]
+    with open('intro.txt', 'r') as file:
+      msg = file.read()
+    if request.method == 'POST':
+      data = request.form['whatever']
+      with open('secret.txt', 'a') as file: #save data permenatly
+        file.write(data + '\n')
+    return render_template('index.html', whatever=data, nums=numbers, intro=msg)
 
 app.run(host='0.0.0.0', port=81)
